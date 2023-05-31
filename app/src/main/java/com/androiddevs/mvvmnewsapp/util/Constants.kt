24f8077befbfb7d8efd07af436dbb3d7bc5cde47
Adrien Abderrahmann <adrienabderrahmann16@gmail.com>
2023-05-31 18:36:59 +0200

    enfin_jarrive_a_commit

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.androiddevs.mvvmnewsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val API_KEY = "1ae49063fcdc4de292a4cb7399a7a6de"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
